a6886e7584f5331fc55973323e78dff42d002af9
zz-22-t <161503379+zz-22-t@users.noreply.github.com>
2024-05-07 12:15:04 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function setup() {
     simplifyThreshold:0
   });
 
-  points2 = font.textToPoints("T O R R E S",50,350,50 ,{
+  points2 = font.textToPoints("T O R R E S",70,300,50 ,{
     sampleFactor:1,
     simplifyThreshold:0
   });
@@ -64,23 +64,24 @@ function draw() {
   
 
   let days_passed = time_passed_ms / (24*60*60*1000);
-  console.log(days_passed,x);
+  ///console.log(days_passed,x);
   if (x >=100){
-    step=0,
+    step=5,
     x=0
   }
   let truncated_days_passed = nf(days_passed, 0, x
     );
-  text(truncated_days_passed+ " days passed since birth", 50, 60)
+  text(truncated_days_passed+"",50,60);
+  text("days passed since birth", 50, 80);
 
 
   ///animate text
   r = pow(1.8,rad);
   for (let i = 0; i < points.length; i++) {
-    ellipse(points[i].x+r*sin(angle + i*10),points[i].y,5,5);
+    ellipse(points[i].x+r*sin(angle + i*15),points[i].y,5,5);
     }
 
-    angle += 1;
+    angle += 5;
     
     ///Torres
     for (let i = 0; i < points2.length; i++) {
@@ -89,6 +90,10 @@ function draw() {
   
       ///angle += 3;
       print(r);
+      if (r >=359){
+        rad=0,
+        r=0
+      }
 }
 
 function mousePressed() {
